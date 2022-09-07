631833f9f659b8044db22eab52236672809a6e48
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-09-07 22:17:51 +0530

    Create run_test.c

---
+++ run_test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Sender.h"
+
+void testfunctions(){
+	tst_BatteryParameters BatteryParam;
+	status_en retStatus = FAILURE;
+
+	//Test generated data for battery paramters is within the range
+	generateSenderData(&BatteryParam);
+	assert(BatteryParam.temperature[20] <= TEMP_MAX);
+	assert(BatteryParam.temperature[1] >= TEMP_MIN);
+	assert(BatteryParam.soc[49] <= SOC_MAX);
+	assert(BatteryParam.soc[15] >= SOC_MIN);
+
+	//Test the Display interface
+	displaySenderData(&retStatus, BatteryParam);
+	assert( retStatus == SUCCESS);
+
+	retStatus = sendBatteryParameters();
+	assert( retStatus == SUCCESS);
+}
+
+int main(void) {
+	sendBatteryParameters();
+	testfunctions();
+}
