f7d9cf3eda0c8a1117817330512e759319eb64d2
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-09-25 23:57:38 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -4,10 +4,7 @@
 int readsensorvalue[5];
 int readsensorvalue1[5];
 int arrNumbers[5] = {0};
-int pos = 0;
-int newAvg = 0;
-int newAvg1 = 0;
-long sum = 0;
+
 int len = sizeof(arrNumbers) / sizeof(int);
 int count = sizeof(readsensorvalue) / sizeof(int);
 
@@ -33,6 +30,7 @@ int simpleMovingAvg(int *arrNumbers, long *sum, int pos, int len, int nextNum)
 
 void newAvgCalculate(int *readsensorvalue,int *readsensorvalue1)
 {
+    pos=newAvg=newAvg1=sum=0;
     for(int i = 0; i < count; i++)
     {
     newAvg = simpleMovingAvg(arrNumbers, &sum, pos, len, readsensorvalue[i]);
