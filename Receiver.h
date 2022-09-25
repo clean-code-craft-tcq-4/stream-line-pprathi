73b932765e87e156e00258a3883a0c908664aca8
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-09-25 23:59:51 +0530

    Update Receiver.h

--- Receiver.h
+++ Receiver.h
@@ -1,8 +1,3 @@
-extern int pos;
-extern int newAvg;
-extern int newAvg1;
-extern long sum;
-
 void readSensorData(int min,int max,int *readsensorvalue,int *readsensorvalue1);
 void displaySensorData(int min,int max,int *readsensordata);
 int simpleMovingAvg(int *arrNumbers, long *sum, int pos, int len, int nextNum);
