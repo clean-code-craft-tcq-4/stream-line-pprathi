134f6189c2d027b54d7c0fc7c2f0a00ddbf3ccfd
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-09-25 23:46:27 +0530

    Update run_test.c

--- run_test.c
+++ run_test.c
@@ -4,6 +4,18 @@
 #include "Sender.h"
 #include "Receiver.h"
 
+int readsensorvalue[5];
+int readsensorvalue1[5];
+int arrNumbers[5] = {0};
+int pos = 0;
+int newAvg = 0;
+int newAvg1 = 0;
+long sum = 0;
+int len = sizeof(arrNumbers) / sizeof(int);
+int count = sizeof(readsensorvalue) / sizeof(int);
+int sampleValue[5] = {1,2,3,4,5};
+int sampleValue1[5] = {1,2,3,4,5};
+
 void testfunctions(){
 	tst_BatteryParameters BatteryParam;
 	status_en retStatus = FAILURE;
