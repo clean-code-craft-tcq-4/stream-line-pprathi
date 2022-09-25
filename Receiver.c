b530f95124a90a8ce9b9ede7a0f05aef455dbbf7
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-09-25 23:15:56 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -1,8 +1,36 @@
-#include<stdio.h>
-void readData()
+#include <stdio.h>
+#include "Receiver.h"
+void readSensorData(int min,int max,int *readsensorvalue,int *readsensorvalue1)
 {
-   int i;
-   printf("Read data\n");
-   scanf("%d",&i);
-   printf("Display read data is %d\n",i);
+    printf("**************Read Sensor Data**************\n");
+    if(readsensorvalue!=NULL)
+    {
+        for(int i = min;i<max;i++)
+        {
+       // scanf("%d,",&readsensorvalue[i]);
+        printf("Read sensor data1:%d,Read sensor data2:%d\n",readsensorvalue[i],readsensorvalue1[i]);
+        }
+    }
+}
+
+int simpleMovingAvg(int *arrNumbers, long *sum, int pos, int len, int nextNum)
+{
+  *sum = *sum - arrNumbers[pos] + nextNum;
+   arrNumbers[pos] = nextNum;
+  return *sum / len;
+}
+
+void newAvgCalculate(int *readsensorvalue,int *readsensorvalue1)
+{
+    for(int i = 0; i < count; i++)
+    {
+    newAvg = simpleMovingAvg(arrNumbers, &sum, pos, len, readsensorvalue[i]);
+    newAvg1 = simpleMovingAvg(arrNumbers, &sum, pos, len, readsensorvalue1[i]);
+    printf("The new Average1 is %d,The new Average2 %d\n", newAvg,newAvg1);
+    pos++;
+    if (pos >= len)
+        {
+            pos = 0;
+        }
+    }
 }
