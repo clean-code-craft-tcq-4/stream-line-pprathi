79f3ba9c29a7903bdec254b6eb97dd57dace9f50
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-10-28 23:17:35 +0530

    Update Receiver.h

--- Receiver.h
+++ Receiver.h
@@ -3,3 +3,4 @@ void displaySensorData(int min,int max,int *readsensordata);
 int simpleMovingAvg(int *arrNumbers, long *sum, int pos, int len, int nextNum);
 void newAvgCalculate(int *readsensorvalue,int *readsensorvalue1);
 int getMinValue(int *arrOfNumbers);
+int getMaxValue(int *arrOfNumbers);
