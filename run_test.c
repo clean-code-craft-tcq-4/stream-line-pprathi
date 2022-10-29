296ecee645fe23ebf4764eb33c99be439be6d655
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-10-29 18:59:54 +0530

    Update run_test.c

--- run_test.c
+++ run_test.c
@@ -4,8 +4,8 @@
 #include "Sender.h"
 #include "Receiver.h"
 
-int sampleValue[5] = {-1,2,3,4,5};
-int sampleValue1[5] = {1,2,3,4,5};
+int sampleValue[50] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50};
+int sampleValue1[50] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50};
 
 void testfunctions(){
 	tst_BatteryParameters BatteryParam;
@@ -32,6 +32,6 @@ int main(void) {
          int retValMin = getMinValue(sampleValue);
          int retValMax= getMaxValue(sampleValue);
          printf("min value=%d,max value=%d",retValMin,retValMax);
-	 readSensorData(0,5,sampleValue,sampleValue1);
+	 readSensorData(0,50,sampleValue,sampleValue1);
          newAvgCalculate(sampleValue,sampleValue1);
 }
