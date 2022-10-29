cbab53495ea9d3a1ae5275dd9be5fc645cedd37f
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-10-29 18:41:35 +0530

    Update Receiver.h

--- Receiver.h
+++ Receiver.h
@@ -6,13 +6,5 @@ extern int getMinValue(int *arrOfNumbers);
 extern int getMaxValue(int *arrOfNumbers);
 extern int readsensorvalue[5];
 extern int readsensorvalue1[5];
-extern int arrNumbers[5] = {0};
-int pos = 0;
-int newAvg = 0;
-int newAvg1 = 0;                                               
-long sum = 0;
-int len = sizeof(arrNumbers) / sizeof(int);
-int count = sizeof(readsensorvalue) / sizeof(int);
-int arrOfNumbers[]={5,13,-12,3,25};
-int len1 = sizeof(arrOfNumbers) / sizeof(int);
+
 
