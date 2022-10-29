dddfc0951343252effee28e665cd4cb2ccc54cef
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-10-29 23:18:20 +0530

    Update run_test.c

--- run_test.c
+++ run_test.c
@@ -6,6 +6,7 @@
 
 int sampleValue[50] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50};
 int sampleValue1[50] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50};
+int sampleValue2[50] = {-1,2,3,4,5,-6,7,8,9,10,11,12,-13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,-29,30,31,32,33,34,35,36,37,-38,39,40,41,-42,43,44,45,46,47,48,49,-50};
 
 void testfunctions(){
 	tst_BatteryParameters BatteryParam;
@@ -33,7 +34,10 @@ int main(void) {
 	 assert(retValMin==1);
          int retValMax= getMaxValue(sampleValue);
 	 assert(retValMax==50);
-         printf("min value=%d,max value=%d",retValMin,retValMax);
+	 int retValMin = getMinValue(sampleValue2);
+	 assert(retValMin==-1);
+	 int retValMax= getMaxValue(sampleValue2);
+         printf("min value=%d,max value=%d\n",retValMin,retValMax);
 	 assert(readSensorData(0,50,sampleValue,sampleValue1)==0);
          assert(newAvgCalculate(sampleValue,sampleValue1)==0);
 }
